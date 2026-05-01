f6c177c0643c8025a62c26e6961afb04298ba679
Boris Velkov <b.velkov@abv.bg>
2026-05-01 11:25:41 +0300

    New Commit

--- src/main/java/com/profile/controller/UserController.java
+++ src/main/java/com/profile/controller/UserController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -31,9 +28,14 @@ public class UserController {
         return "all-users";
     }
 
-    @GetMapping("/")
-    public String getIndexPage() {
-        return "index";
+    @Controller
+    public class HomeController {
+
+        @GetMapping("/")
+        @ResponseBody
+        public String home() {
+            return "WORKS";
+        }
     }
 
     @GetMapping("/login")
