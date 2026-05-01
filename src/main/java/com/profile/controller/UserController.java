c2ea69c61c8ef0fa5eea0706de510bee9b11563f
Boris Velkov <b.velkov@abv.bg>
2026-05-01 12:01:51 +0300

    Edited getHomePage in Controller

--- src/main/java/com/profile/controller/UserController.java
+++ src/main/java/com/profile/controller/UserController.java
@@ -6,7 +6,10 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,14 +31,9 @@ public class UserController {
         return "all-users";
     }
 
-    @Controller
-    public class HomeController {
-
-        @GetMapping("/")
-        @ResponseBody
-        public String home() {
-            return "WORKS";
-        }
+    @GetMapping("/")
+    public String getIndexPage() {
+        return "index";
     }
 
     @GetMapping("/login")
