e19292da0e1b56df9173fe2a383e0eef0bb3595f
Boris Velkov <b.velkov@abv.bg>
2026-05-22 07:00:57 +0300

    New Commit

--- src/test/java/com/profile/service/serviceImpl/ProjectServiceImplTest.java
+++ src/test/java/com/profile/service/serviceImpl/ProjectServiceImplTest.java
@@ -1,9 +1,13 @@
 package com.profile.service.serviceImpl;
 
 import com.profile.repository.ProjectRepository;
+import com.profile.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
 @ExtendWith(MockitoExtension.class)
 public class ProjectServiceImplTest {
@@ -13,4 +17,27 @@ public class ProjectServiceImplTest {
 
     @Mock
     private ProjectRepository mockProjectRepository;
+
+    @Mock
+    private UserRepository mockUserRepository;
+    @Mock
+    private ModelMapper mockModelMapper;
+
+    @BeforeEach
+    void setUp(){
+        mockProjectService = new ProjectServiceImpl(
+                mockProjectRepository,
+                mockUserRepository,
+                mockModelMapper
+        );
+    }
+
+    @Test
+    void getMyAllProjects() {
+
+    }
+
+    @Test
+    void addProject() {
+    }
 }
