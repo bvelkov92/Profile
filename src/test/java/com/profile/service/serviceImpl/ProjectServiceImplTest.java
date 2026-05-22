452620cb4460909008dd8ad22c249290101e26d0
Boris Velkov <b.velkov@abv.bg>
2026-05-22 08:02:58 +0300

    New Test added.

--- src/test/java/com/profile/service/serviceImpl/ProjectServiceImplTest.java
+++ src/test/java/com/profile/service/serviceImpl/ProjectServiceImplTest.java
@@ -1,10 +1,10 @@
 package com.profile.service.serviceImpl;
 
+import com.profile.models.dto.projectDTO.AddNewProjectDTO;
 import com.profile.models.entity.Project;
 import com.profile.models.entity.User;
 import com.profile.repository.ProjectRepository;
 import com.profile.repository.UserRepository;
-import org.aspectj.weaver.patterns.PerObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,12 +12,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -36,6 +38,10 @@ public class ProjectServiceImplTest {
     @Mock
     private ModelMapper mockModelMapper;
 
+    Project project = new Project();
+    Project project2 = new Project();
+    User creator = new User();
+
     @BeforeEach
     void setUp(){
         mockProjectService = new ProjectServiceImpl(
@@ -43,6 +49,13 @@ public class ProjectServiceImplTest {
                 mockUserRepository,
                 mockModelMapper
         );
+        creator.setUsername("regUser");
+
+        project.setProjectName("myProjectOneForTest");
+        project.setProjectCreator(creator);
+
+        project2.setProjectName("myProjectOneForTest2");
+        project2.setProjectCreator(creator);
     }
 
     @Test
@@ -50,17 +63,6 @@ public class ProjectServiceImplTest {
         Authentication authentication = mock(Authentication.class);
         SecurityContext securityContext = mock(SecurityContext.class);
         SecurityContextHolder.setContext(securityContext);
-
-        User creator = new User();
-        creator.setUsername("regUser");
-
-        Project project = new Project();
-        project.setProjectCreator(creator);
-        project.setProjectName("myProjectOneForTest");
-
-        Project project2 = new Project();
-        project2.setProjectCreator(creator);
-        project2.setProjectName("myProjectOneForTest2");
 
         List <Project> allProjects = new ArrayList<>();
         allProjects.add(project);
@@ -79,5 +81,34 @@ public class ProjectServiceImplTest {
 
     @Test
     void addProject() {
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        SecurityContextHolder.setContext(securityContext);
+
+        AddNewProjectDTO projectWithImage = new AddNewProjectDTO();
+        projectWithImage.setProjectName("The most new project with image");
+
+        MockMultipartFile mockFile = new MockMultipartFile(
+                "projectImage",          // name
+                "image.png",             // original filename
+                "image/png",             // content type
+                "fake image content".getBytes()
+        );
+        //TODO: Да се провери, защо не минава.
+//
+//        AddNewProjectDTO projectNoImage = new AddNewProjectDTO();
+//        projectNoImage.setProjectName("The most new project with image");
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("regUser");
+        when(mockUserRepository.findByUsername(authentication.getName())).thenReturn(Optional.of(creator));
+        mockProjectService.addProject(projectWithImage,mockFile);
+
+        List<Project> myAllProjects = mockProjectService.getMyAllProjects();
+
+
+        Assertions.assertEquals(3, myAllProjects.size());
+
+
     }
 }
