d9934e877ea0b9adf6b9822739e5b845a57bb712
Boris Velkov <b.velkov@abv.bg>
2026-05-31 08:19:19 +0300

    BlackList Tests

---
+++ src/test/java/com/profile/service/serviceImpl/BlackListServiceImplTest.java
@@ -0,0 +1,76 @@
+package com.profile.service.serviceImpl;
+
+import com.profile.models.entity.BlackListUser;
+import com.profile.models.entity.User;
+import com.profile.models.enums.RolesEnum;
+import com.profile.repository.BlackListRepository;
+import com.profile.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class BlackListServiceImplTest {
+
+    @Mock
+    private BlackListServiceImpl mockBlackListService;
+
+    @Mock
+    private BlackListRepository mockBlackListRepository;
+    @Mock
+    private UserRepository mockUserRepository;
+
+    @Mock
+    private ModelMapper mockModelMapper;
+
+    private User mockedUser;
+
+    @BeforeEach
+    void setUp(){
+        mockBlackListService = new BlackListServiceImpl( mockUserRepository, mockBlackListRepository,mockModelMapper);
+
+        mockedUser = new User();
+        mockedUser.setUsername("username");
+        mockedUser.setBanned(false);
+        mockedUser.setRole(RolesEnum.USER);
+
+    }
+
+    @Test
+    void addUserToBlackList() {
+        BlackListUser mockedBlackListSUser = new BlackListUser();
+        String bannedUser = "username";
+
+        when(mockBlackListRepository.findByBannedUser_Username(bannedUser)).thenReturn(Optional.empty());
+        when(mockModelMapper.map(mockedUser, BlackListUser.class)).thenReturn(mockedBlackListSUser);
+
+        mockBlackListService.addUserToBlackList(mockedUser);
+
+        verify(mockBlackListRepository).save(mockedBlackListSUser);
+    }
+
+    @Test
+    void deleteUserFromBlackList() {
+    }
+
+    @Test
+    void getUserRepository() {
+    }
+
+    @Test
+    void getBlackListRepository() {
+    }
+
+    @Test
+    void getModelMapper() {
+    }
+}
